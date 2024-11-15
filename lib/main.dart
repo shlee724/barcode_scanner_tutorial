1e8fbbfb3c833d39b09baf121fecb3e728c7362d
paperman1 <powerplant724@gmail.com>
2024-11-15 23:17:52 +0900

    Added Codes To Improve Scan Accuracy

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,7 @@ class BarcodeScannerScreen extends StatefulWidget {
 
 class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
   final MobileScannerController cameraController = MobileScannerController();
+  List<String?> scannedBarcodes = [];
 
   @override
   void dispose() {
@@ -86,11 +87,27 @@ class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
         onDetect: (BarcodeCapture capture) async {
           final barcode = capture.barcodes.first;
           final String? code = barcode.rawValue;
-          if (code != null) {
-            // 카메라를 중지하고, 결과를 팝업으로 전달
-            await cameraController.stop();
-            Navigator.pop(context, code);
+
+          if (scannedBarcodes.length <3) {     //코드 길이가 10을 넘지 않으면 리스트에 코드만 추가
+            scannedBarcodes.add(code);
+          } else {                              //코드 길이가 10이 되면 바코드를 제대로 인식했는지 검사하는 알고리즘 돌아감
+
+            if (scannedBarcodes[0] != null){
+              bool isEightDigitNumber = RegExp(r'^\d{8}$').hasMatch(scannedBarcodes[0]!); // 바코드가 8자리의 숫자로만 이루어져 있는지 판단하는 코드
+              if (isEightDigitNumber && scannedBarcodes.every((element) => element == scannedBarcodes[0])) {  //8자리 숫자 and 인식한 모든 리스트의 바코드가 동일
+                print(scannedBarcodes);
+                final codeResult = scannedBarcodes[0];
+                scannedBarcodes = [];   //리스트 초기화
+                // 카메라를 중지하고, 결과를 팝업으로 전달
+                await cameraController.stop();
+                Navigator.pop(context, codeResult);
+              }
+            }
+            scannedBarcodes.removeAt(0);
+            scannedBarcodes.add(code);
+
           }
+
         },
       ),
     );
