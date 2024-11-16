865e9d6b44c0a27a4d415f6e823ff0cea7ae6d10
paperman1 <powerplant724@gmail.com>
2024-11-16 16:53:15 +0900

    Added Codes To Restrict Scanning Only code39 Barcode Formats

--- lib/main.dart
+++ lib/main.dart
@@ -87,6 +87,11 @@ class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
         onDetect: (BarcodeCapture capture) async {
           final barcode = capture.barcodes.first;
           final String? code = barcode.rawValue;
+          final format = barcode.format;
+          print("barcode type: $format");
+          if (format == BarcodeFormat.code39){
+            print(true);
+          }
 
           if (scannedBarcodes.length <3) {     //코드 길이가 10을 넘지 않으면 리스트에 코드만 추가
             scannedBarcodes.add(code);
@@ -94,7 +99,9 @@ class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
 
             if (scannedBarcodes[0] != null){
               bool isEightDigitNumber = RegExp(r'^\d{8}$').hasMatch(scannedBarcodes[0]!); // 바코드가 8자리의 숫자로만 이루어져 있는지 판단하는 코드
-              if (isEightDigitNumber && scannedBarcodes.every((element) => element == scannedBarcodes[0])) {  //8자리 숫자 and 인식한 모든 리스트의 바코드가 동일
+              if (isEightDigitNumber
+                  && scannedBarcodes.every((element) => element == scannedBarcodes[0]
+                  && format == BarcodeFormat.code39)) {  //8자리 숫자 and 인식한 모든 리스트의 바코드가 동일 and 바코드 타입이 code39
                 print(scannedBarcodes);
                 final codeResult = scannedBarcodes[0];
                 scannedBarcodes = [];   //리스트 초기화
