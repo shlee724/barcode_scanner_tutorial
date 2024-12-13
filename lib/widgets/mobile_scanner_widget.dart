aca843ad58b64bd09cd1c7dd41295066bfa1a7c5
paperman1 <powerplant724@gmail.com>
2024-12-13 21:20:06 +0900

    Refactored _handleBarcodeDetection Function

--- lib/widgets/mobile_scanner_widget.dart
+++ lib/widgets/mobile_scanner_widget.dart
@@ -9,37 +9,39 @@ class MobileScannerWidget extends StatelessWidget {
     cameraController.dispose();
   }
 
+  //8자리 숫자 and 인식한 모든 리스트의 바코드가 동일 and 바코드 타입이 code39인지 검사
+  bool isScannedBarcodeValid(BarcodeFormat format) {
+    if (scannedBarcodes[0] != null) {
+      bool isEightDigitNumber =
+          RegExp(r'^\d{8}$').hasMatch(scannedBarcodes[0]!);
+      if (isEightDigitNumber &&
+          scannedBarcodes.every((element) =>
+              element == scannedBarcodes[0] &&
+              format == BarcodeFormat.code39)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   void _handleBarcodeDetection(
       BarcodeCapture capture, BuildContext context) async {
     final barcode = capture.barcodes.first;
     final String? code = barcode.rawValue;
     final format = barcode.format;
-    print("barcode type: $format");
-    if (format == BarcodeFormat.code39) {
-      print(true);
-    }
 
     if (scannedBarcodes.length < 3) {
-      //코드 길이가 10을 넘지 않으면 리스트에 코드만 추가
+      //바코드가 10번 찍히지 않았으면 찍힌 리스트에 바코드 추가
       scannedBarcodes.add(code);
     } else {
-      //코드 길이가 10이 되면 바코드를 제대로 인식했는지 검사하는 알고리즘 돌아감
+      // 찍힌 바코드가 10개 이상이면 바코드를 제대로 인식했는지 검사하는 알고리즘
 
-      if (scannedBarcodes[0] != null) {
-        bool isEightDigitNumber = RegExp(r'^\d{8}$')
-            .hasMatch(scannedBarcodes[0]!); // 바코드가 8자리의 숫자로만 이루어져 있는지 판단하는 코드
-        if (isEightDigitNumber &&
-            scannedBarcodes.every((element) =>
-                element == scannedBarcodes[0] &&
-                format == BarcodeFormat.code39)) {
-          //8자리 숫자 and 인식한 모든 리스트의 바코드가 동일 and 바코드 타입이 code39
-          print(scannedBarcodes);
-          final codeResult = scannedBarcodes[0];
-          scannedBarcodes = []; //리스트 초기화
-          // 카메라를 중지하고, 결과를 팝업으로 전달
-          await cameraController.stop();
-          Navigator.pop(context, codeResult);
-        }
+      if (isScannedBarcodeValid(format)) {
+        final codeResult = scannedBarcodes[0];
+        scannedBarcodes = []; //리스트 초기화
+        // 카메라를 중지하고, 결과를 팝업으로 전달
+        await cameraController.stop();
+        Navigator.pop(context, codeResult);
       }
       scannedBarcodes.removeAt(0);
       scannedBarcodes.add(code);
